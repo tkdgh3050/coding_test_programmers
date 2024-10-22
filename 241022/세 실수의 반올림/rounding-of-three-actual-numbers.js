1d1a8a73c67a0a56ef49dfec46357a678c99a6d8
Sangho Kim <tkdgh3050@gmail.com>
2024-10-22 15:34:14 +0900

    Title: 세 실수의 반올림, Time: 229ms, Memory: 14MB, Status: Passed - CodeTree

--- 241022/세 실수의 반올림/rounding-of-three-actual-numbers.js
+++ 241022/세 실수의 반올림/rounding-of-three-actual-numbers.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().split('\n');
 
-for (let x = 0; x < input.length; x++) {
-    console.log(Number(input[x]).toFixed(3))
-}
+    console.log(Number(input[0]).toFixed(3))
+    console.log(Number(input[1]).toFixed(3))
+    console.log(Number(input[2]).toFixed(3))
