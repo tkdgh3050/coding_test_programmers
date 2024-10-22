8a7fc43a723d5cacbdcbaa5a38e1662c0214e1c2
Sangho Kim <tkdgh3050@gmail.com>
2024-10-22 20:32:17 +0900

    Title: 알파벳에 따른 평가, Time: 169ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241022/알파벳에 따른 평가/evaluation-by-alphabet.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const val = fs.readFileSync(0).toString();
+
+if (val === 'S') {
+    console.log('Superior')
+} else if (val === 'A') {
+    console.log('Excellent')
+} else if (val === 'B') {
+    console.log('Good')
+} else if (val === 'C') {
+    console.log('Usually')
+} else if (val === 'D') {
+    console.log('Effort')
+} else {
+    console.log('Failure')
+}
