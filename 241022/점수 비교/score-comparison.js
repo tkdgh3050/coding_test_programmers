6fd8cf990f4ec5bff94c5ec465018c37959ef85d
Sangho Kim <tkdgh3050@gmail.com>
2024-10-22 21:00:40 +0900

    Title: 점수 비교, Time: 280ms, Memory: 14MB, Status: Passed - CodeTree

--- 241022/점수 비교/score-comparison.js
+++ 241022/점수 비교/score-comparison.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const val = fs.readFileSync(0).toString().split('\n');
 
-const math = val[0].trim().split(' ').map(v => Number(v))
-const eng = val[1].trim().split(' ').map(v => Number(v))
+const A = val[0].trim().split(' ').map(v => Number(v))
+const B = val[1].trim().split(' ').map(v => Number(v))
 
-math[0] >= math[1] && eng[0] >= eng[1] ? console.log(1) : console.log(0)
+A[0] > B[0] && A[1] > B[1] ? console.log(1) : console.log(0)
