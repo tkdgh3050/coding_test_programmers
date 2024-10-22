142a2a4af0aeb34c0ad2b3b6aace171f2a75a440
Sangho Kim <tkdgh3050@gmail.com>
2024-10-22 20:59:15 +0900

    Title: 점수 비교, Time: 163ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 241022/점수 비교/score-comparison.js
+++ 241022/점수 비교/score-comparison.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const val = fs.readFileSync(0).toString().split('\n');
 
-const math = val[0].split(' ').map(v => Number(v))
-const eng = val[1].split(' ').map(v => Number(v))
+const math = val[0].trim().split(' ').map(v => Number(v))
+const eng = val[1].trim().split(' ').map(v => Number(v))
 
 math[0] >= math[1] && eng[0] >= eng[1] ? console.log(1) : console.log(0)
