c5fdfc1b8fec3c157da05aa9ff6f2a31642688b7
Sangho Kim <tkdgh3050@gmail.com>
2024-10-23 15:45:08 +0900

    Title: 격자 모양 출력하기, Time: 207ms, Memory: 14MB, Status: Passed - CodeTree

--- 241023/격자 모양 출력하기/print-grid-shape.js
+++ 241023/격자 모양 출력하기/print-grid-shape.js
@@ -3,7 +3,7 @@ let [info, ...val] = fs.readFileSync(0).toString().trim().split("\n");
 
 const [n, m] = info.trim().split(' ').map(Number);
 const dot = val.map(line => line.trim().split(' ').map(Number));
-const answer = Array.from({length:n}, () => Array(m).fill(0));
+const answer = Array.from({length:n}, () => Array(n).fill(0));
 
 for (let [x,y] of dot) {
     answer[x-1][y-1] = x * y;
