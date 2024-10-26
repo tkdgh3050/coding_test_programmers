0935523d329f82f92468413a91b7e779cb234131
Sangho Kim <tkdgh3050@gmail.com>
2024-10-26 14:17:25 +0900

    Title: 삼각형 컨베이어 벨트, Time: 185ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241026/삼각형 컨베이어 벨트/conveyor-belt-triangle.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const [input1, ...input2] = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n,t] = input1.trim().split(' ').map(Number);
+const arr = input2.map(val => val.trim().split(' ').map(Number));
+
+for (let x = 1; x <= t; x++) {
+    [arr[0], arr[1], arr[2]] = [
+        [arr[2][n-1], ...arr[0].slice(0, n-1)],
+        [arr[0][n-1], ...arr[1].slice(0, n-1)],
+        [arr[1][n-1], ...arr[2].slice(0, n-1)]
+    ]
+}
+
+for (let x = 0; x < n; x++) {
+    console.log(...arr[x])
+}
