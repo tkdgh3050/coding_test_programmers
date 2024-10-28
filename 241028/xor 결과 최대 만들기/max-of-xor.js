93655f5ca52cbc97b549279ddd97d19416bb4a21
Sangho Kim <tkdgh3050@gmail.com>
2024-10-28 14:29:38 +0900

    Title: xor 결과 최대 만들기, Time: 230ms, Memory: 47MB, Status: Passed - CodeTree

--- 241028/xor 결과 최대 만들기/max-of-xor.js
+++ 241028/xor 결과 최대 만들기/max-of-xor.js
@@ -4,13 +4,16 @@ const [info, val] = fs.readFileSync(0).toString().trim().split('\n');
 const [n,m] = info.trim().split(' ').map(Number);
 const arr = val.trim().split(' ').map(Number);
 let max = 0;
+const temp = []
 
-function getComb(now, list) {
-    if (list.length === m) {
-        max = Math.max(max, list.reduce((a,c) => a ^ c, 0))
+function getComb(now) {
+    if (temp.length === m) {
+        max = Math.max(max, temp.reduce((a,c) => a ^ c, 0))
     } else {
         for (let x = now; x < arr.length; x++) {
-            getComb(x+1, [...list, arr[x]]);
+            temp.push(arr[x]);
+            getComb(x+1);
+            temp.pop();
         }
     }
 }
