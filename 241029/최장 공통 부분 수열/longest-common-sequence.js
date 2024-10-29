bc03626f15dc5d36ea3157913c06de07391c5d4b
Sangho Kim <tkdgh3050@gmail.com>
2024-10-29 20:43:10 +0900

    Title: 최장 공통 부분 수열, Time: 254ms, Memory: 47MB, Status: Passed - CodeTree

--- 241029/최장 공통 부분 수열/longest-common-sequence.js
+++ 241029/최장 공통 부분 수열/longest-common-sequence.js
@@ -4,30 +4,28 @@
 
 const fs = require('fs');
 const [str1, str2] = fs.readFileSync(0).toString().trim().split('\n');
-const dp = Array.from({length: Math.max(str1.length, str2.length)}, () => 0)
+const dp = Array.from({length: str1.length}, () => Array(str2.length).fill(0));
 
-let idx1 = 0 
-let idx2 = 0;
-
-if (str1[idx1] === str2[idx2]) {
-    dp[0] = 1;
-    idx1 += 1;
-    idx2 += 1;
+let temp = 0;
+for (let x = 0; x < str1.length; x++) {
+    if (temp === 0 && str2[0] === str1[x]) temp = 1;
+    dp[x][0] = temp;
+}
+temp = 0;
+for (let y = 0; y < str2.length; y++) {
+    if (temp === 0 && str1[0] === str2[y]) temp = 1;
+    dp[0][y] = temp;
 }
 
-for (let x = 1; x < dp.length; x++) {
-    let cnt = 0;
-    for (let one = idx1; one <= x; one++) {
-        for (let two = idx2; two <= x; two++) {
-            if (str1[one] === str2[two]) {
-                idx1 = one+1;
-                idx2 = two+1;
-                cnt += 1;
-                break;
-            }
+for (let x = 1; x < str1.length; x++) {
+    for (let y = 1; y < str2.length; y++) {
+        if (str1[x] === str2[y]) {
+            dp[x][y] = dp[x-1][y-1] + 1;
+        } else {
+            dp[x][y] = Math.max(dp[x-1][y], dp[x][y-1])
         }
+        
     }
-    dp[x] = dp[x-1] + cnt
 }
 
-console.log(dp[dp.length-1])
+console.log(dp[str1.length-1][str2.length-1])
