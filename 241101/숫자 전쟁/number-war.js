a298473411c81913d714ad591ed26145ddc93562
Sangho Kim <tkdgh3050@gmail.com>
2024-11-01 16:25:17 +0900

    Title: 숫자 전쟁, Time: 197ms, Memory: 20MB, Status: Passed - CodeTree

--- 241101/숫자 전쟁/number-war.js
+++ 241101/숫자 전쟁/number-war.js
@@ -1,44 +1,45 @@
-// 카드게임
-// 2명이 플레이
-// 각자 자연수 적히 카드 가지고 시작
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
 
-// 둘 중 하나 선택 (카드는 쉬프트 해서 진행)
-    // 대결
-        // 카드 비교해서 진사람이 본인 카드 숫자만큼 점수획득
-        // 작은 사람 카드 버림
-        // 같은 점수 시 둘다 버림
-    // 버리기
-        // 둘다 버림
-// 한쪽이 카드 모두 소진 시 종료
-// 얻은 점수 합으로 최종결과
-// 내 최고 점수 출력
-// dfs
+// 각 플레이어의 카드 정보를 입력받습니다.
+const n = Number(input[0]);
+const a = [0].concat(input[1].split(' ').map(Number)).concat([0]);
+const b = [0].concat(input[2].split(' ').map(Number)).concat([0]);
 
-const fs = require('fs');
-const [info, ...data] = fs.readFileSync(0).toString().trim().split('\n');
+// dp 배열을 초기화합니다. 초기값은 -1로 설정합니다.
+const dp = Array.from(Array(n + 5), () => Array(n + 5).fill(-1));
 
-const n = Number(info);
-const arr = data.map(v => v.trim().split(' ').map(Number));
-const arr1 = arr[0];
-const arr2 = arr[1];
+// 기본 케이스를 설정합니다.
+dp[0][0] = 0;
 
-let maxScore = 0;
+// 각 경우의 수를 동적 프로그래밍으로 계산합니다.
+// dp[i][j] :: 첫 번째 플레이어는 i번 카드까지, 두 번째 플레이어는 j번 카드까지 버렸을 때 나올 수 있는 최대 점수
+for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+        if (dp[i][j] === -1) {
+            continue;
+        }
 
-function DFS(idx1, idx2, score) {
-    if (idx1 >= n || idx2 >= n) {
-        maxScore = Math.max(maxScore, score);
-    } else {
-        // 싸우기
-        if (arr1[idx1] > arr2[idx2]) {
-            DFS(idx1, idx2+1, score + arr2[idx2])
-        } else if (arr1[idx1] < arr2[idx2]) {
-            DFS(idx1+1, idx2, score);
-        } 
-        // 버리기
-        DFS(idx1+1, idx2+1, score);
+        // 카드 대결 - 첫 번째 플레이어의 카드가 더 작은 경우
+        if (a[i + 1] < b[j + 1]) {
+            dp[i + 1][j] = Math.max(dp[i + 1][j], dp[i][j]);
+        }
+
+        // 카드 대결 - 두 번째 플레이어의 카드가 더 작은 경우
+        if (a[i + 1] > b[j + 1]) {
+            dp[i][j + 1] = Math.max(dp[i][j + 1], dp[i][j] + b[j + 1]);
+        }
+
+        // 카드 버리기
+        dp[i + 1][j + 1] = Math.max(dp[i + 1][j + 1], dp[i][j]);
     }
 }
 
-DFS(0,0,0)
+// 결과를 계산하여 출력합니다.
+let ans = 0;
+for (let i = 0; i <= n; i++) {
+    ans = Math.max(ans, dp[i][n]);
+    ans = Math.max(ans, dp[n][i]);
+}
 
-console.log(maxScore)
+console.log(ans);
