63ca1069f5b753274cc834c13837914fbf62b4f3
Sangho Kim <tkdgh3050@gmail.com>
2024-11-07 18:45:14 +0900

    Title: 겹치지 않게 선분 고르기, Time: 168ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241107/겹치지 않게 선분 고르기/select-segments-without-overlap.js
@@ -0,0 +1,38 @@
+// dfs 이용해서 경우의 수 뽑고 최대 갯수 기록
+
+const fs = require('fs');
+const [info, ...data] = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(info);
+const lineArr = data.map(v => v.trim().split(' ').map(Number));
+let maxVal =0;
+
+const dfs = (level, arr) => {
+    if (level === n) {
+        maxVal = Math.max(maxVal, checkCnt(arr))
+    } else {
+        dfs(level+1, [...arr, lineArr[level]])
+        dfs(level+1, [...arr])
+    }
+}
+
+const checkCnt = (arr) => {
+    const temp = []
+    for (let [x1, x2] of arr) {
+        temp.push([x1, 1])
+        temp.push([x2, -1])
+    }
+    temp.sort((a,b) => a[0] - b[0]);
+    let cnt = 0;
+    let total = 0;
+    for (let [_, val] of temp) {
+        total += val;
+        if (total > 1) return 0;
+        if (total === 0) cnt += 1;
+    }
+    return cnt
+}
+
+dfs(0,[]);
+
+console.log(maxVal)
