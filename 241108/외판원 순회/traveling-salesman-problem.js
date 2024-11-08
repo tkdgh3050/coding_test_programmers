c4fd3f7f48e129fcfe2f0ce4199f4ed0e324d1c6
Sangho Kim <tkdgh3050@gmail.com>
2024-11-08 11:16:22 +0900

    Title: 외판원 순회, Time: 178ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241108/외판원 순회/traveling-salesman-problem.js
@@ -0,0 +1,33 @@
+// 1번에서 시작
+// 모든지점을 한번씩만 방문하고 다시 1번으로
+// i->j 비용있음
+// 모든 지점을 겹치지 않게 방문하고 되돌아오는 최소 비용 합
+// n이 크지 않으므로 DFS로 순서쌍 찾기
+
+const fs = require('fs');
+const [info, ...data] = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(info);
+const arr = data.map(v => v.trim().split(' ').map(Number));
+
+const visited = Array.from({length: n}, () => false);
+let minSum = Number.MAX_SAFE_INTEGER;
+const DFS = (level, cost, nowIdx) => {
+    if (level === n-1) {
+        // 마지막 순서에서 0로 돌아가는 로직 추가
+        minSum = Math.min(minSum, cost + arr[nowIdx][0])
+    } else {
+        for (let idx = 1; idx < n; idx++) {
+            if (!visited[idx]) {
+                visited[idx] = true;
+                DFS(level+1, cost + arr[nowIdx][idx] ,idx);
+                visited[idx] = false;
+            }
+        }
+    }
+}
+
+visited[0] = true
+DFS(0, 0, 0)
+
+console.log(minSum)
