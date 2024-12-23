c848bfffac69ae925569e2ebd50191908d18bc4c
Sangho Kim <tkdgh3050@gmail.com>
2024-12-23 10:57:57 +0900

    Title: 올바른 등식 만들기, Time: 186ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241223/올바른 등식 만들기/right-equality.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const [info, data] = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n,m] = info.trim().split(' ').map(Number);
+const arr = data.trim().split(' ').map(Number);
+
+let dpMap = new Map();
+
+for (let val of arr) {
+    if(dpMap.size === 0) {
+        dpMap.set(val, 1)
+        dpMap.set(-val, 1)
+    } else {
+        const tempMap = new Map();
+        for (let dp of dpMap.keys()) {
+            tempMap.has(dp+val) ? tempMap.set(dp+val, dpMap.get(dp) + tempMap.get(dp+val)) : tempMap.set(dp+val, dpMap.get(dp))
+            tempMap.has(dp-val) ? tempMap.set(dp-val, dpMap.get(dp) + tempMap.get(dp-val)) : tempMap.set(dp-val, dpMap.get(dp))
+        }
+        dpMap = tempMap;
+    }
+}
+
+console.log(dpMap.has(m) ? dpMap.get(m) : 0)
